306bb7a0e652441f8a432bb2a8d8079801036e1b
YogurtRaitha <eshan.rathi@outlook.com>
2022-02-18 22:48:02 -0800

    music not being read or smth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const {Client, Intents, Message, CommandInteractionOptionResolver} = require("discord.js")
-const {joinVoiceChannel, createAudioPlayer, createAudioResource} = require("@discordjs/voice")
-const TOKEN = "OTMzNjAxNjUxNDgxMjU1OTg2.Yej6Sw.0mrR8LkM8xVrm5YpofGEybbeKNA"
+const {joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType} = require("@discordjs/voice")
+const {createReadStream} = require('fs')
+const TOKEN = "OTMzNjAxNjUxNDgxMjU1OTg2.Yej6Sw.yjVAJSupdL5xZGog_Nk2PYt2kFw"
 require("dotenv").config()
 const client = new Client({
     intents:["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES"]
@@ -25,23 +26,36 @@ client.on("messageCreate", (message) => {
             })
             try {
                 const player = createAudioPlayer()
-                const resource = createAudioResource("./doorbell-5.mp3")               
+                // An AudioPlayer will always emit an "error" event with a .resource property
+                player.on('error', error => {
+                    console.error('Error:', error.message, 'with track', error.resource.metadata.title);
+                });
+
+                const resource = createAudioResource(createReadStream("./testdoorbell.ogg"), {
+                    inputType: StreamType.OggOpus,
+                    metadata: {
+                        title: 'doorbell'
+                    }
+                })               
+
+
+                connection.subscribe(player)
                 player.play()
-                const dispatcher = connection.subscribe(player)
+                // const dispatcher = connection.subscribe(player)
                 
-                dispatcher.on('start', () => {
-                    console.log('Now playing ');
-                    const statusEmbed = new Discord.MessageEmbed()
-                    .addField('Now Playing', "ding dong")
-                    .setColor('#0066ff')
-                });
+                // dispatcher.on('start', () => {
+                //     console.log('Now playing ');
+                //     const statusEmbed = new Discord.MessageEmbed()
+                //     .addField('Now Playing', "ding dong")
+                //     .setColor('#0066ff')
+                // });
                 
-                dispatcher.on('error', console.error);
+                // dispatcher.on('error', console.error);
             
-                dispatcher.on('finish', () => {
-                    console.log('Music has finished playing.')
-                    connection.destroy()
-                });
+                // dispatcher.on('finish', () => {
+                //     console.log('Music has finished playing.')
+                //     connection.destroy()
+                // });
                 
             } catch(ex) {
                 console.log(ex)
